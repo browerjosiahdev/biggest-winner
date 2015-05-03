277a6655de6d970a6f4129557c581bb5db7e6494
Josiah Brower <josiahbrower@Josiahs-MacBook-Pro.local>
2015-05-03 09:58:09 -0600

    Setup the scriptures post page

--- js/site.js
+++ js/site.js
@@ -1,9 +1,26 @@
-var m_masterApp = angular.module('MasterApp', []);
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Group: Apps.
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+var m_masterApp     = angular.module('MasterApp', []);
+var m_postsApp      = angular.module('PostsApp', []);
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Group: Controllers.
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 m_masterApp.controller('MasterBodyCtrl', ['$scope', '$http', function( $scope, $http ) 
 {
     $http.get('data/points.json').success(function( data ) 
     {
         $scope.points = data;
+    });
+}]);
+
+m_postsApp.controller('PostsBodyCtrl', ['$scope', '$http', function( $scope, $http )
+{
+    $http.get('data/scriptures.json').success(function( data )
+    {
+        $scope.posts = data;
     });
 }]);
