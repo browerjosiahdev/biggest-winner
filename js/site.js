c7f6c6ec7687b41dcd96c1aca93153cfe7d63d2b
Josiah Brower <josiahbrower@Josiahs-MacBook-Pro.local>
2015-05-02 22:53:38 -0600

    Update styles for the home page

--- js/site.js
+++ js/site.js
@@ -1,7 +1,9 @@
 var m_masterApp = angular.module('MasterApp', []);
 
-m_masterApp.controller('MasterBodyCtrl', ['$scope', '$http', function( $scope, $http ) {
-    $http.get('data/points.json').success(function( data ) {
+m_masterApp.controller('MasterBodyCtrl', ['$scope', '$http', function( $scope, $http ) 
+{
+    $http.get('data/points.json').success(function( data ) 
+    {
         $scope.points = data; 
-    });        
+    });
 }]);
