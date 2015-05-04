04360b986171f55720f854b81e21aef1e82bfd46
Josiah Brower <josiahbrower@Josiahs-MacBook-Pro.local>
2015-05-04 00:19:52 -0600

    Made updates to sql file to create points, and user points table. Made style updates to the filters for the scriptures page

--- _dev/mysql/biggest_winner_godaddy.sql
+++ _dev/mysql/biggest_winner_godaddy.sql
@@ -14,3 +14,31 @@ CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT,
                                  
 INSERT IGNORE INTO users (name, login, password, email) 
                   VALUE ('Administrator', 'sysadmin', 'Passw0rd', 'browerjosiah@gmail.com');
+                  
+CREATE TABLE IF NOT EXISTS points (id INT AUTO_INCREMENT, 
+                                   type VARCHAR(100) NOT NULL, 
+                                   
+                                   UNIQUE(type), 
+                                   
+                                   PRIMARY KEY(id));
+                                   
+INSERT IGNORE INTO points (type)
+                    VALUE ('scriptures');
+INSERT IGNORE INTO points (type)
+                    VALUE ('exercise');
+INSERT IGNORE INTO points (type)
+                    VALUE ('water');
+INSERT IGNORE INTO points (type)
+                    VALUE ('treats');
+                    
+CREATE TABLE IF NOT EXISTS users_points (id INT AUTO_INCREMENT, 
+                                         user_id INT NOT NULL, 
+                                         point_id INT NOT NULL, 
+                                         date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+                                         
+                                         PRIMARY KEY (id));
+                                         
+/* START: DEVELOPMENT */
+INSERT IGNORE users_points (user_id, point_id)
+                     VALUE (1, 1);
+/* END: DEVELOPMENT */                     
