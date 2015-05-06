47bb0e0c64b8353367d4eed90bf6ec1da70ba77b
Josiah Brower <josiahbrower@Josiahs-MacBook-Pro.local>
2015-05-06 01:58:19 -0600

    Updated points page to be static

--- js/modules.js
+++ js/modules.js
@@ -2,20 +2,11 @@
 // Group: Apps.
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-var m_masterApp     = angular.module('MasterApp', []);
-var m_postsApp      = angular.module('PostsApp', []);
+var m_postsApp = angular.module('PostsApp', []);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Group: Controllers.
 /////////////////////////////////////////////////////////////////////////////////////////////
-
-m_masterApp.controller('MasterBodyCtrl', ['$scope', '$http', function( $scope, $http ) 
-{
-    $http.get('data/points.json').success(function( data ) 
-    {
-        $scope.points = data;
-    });
-}]);
 
 m_postsApp.controller('PostsBodyCtrl', ['$scope', '$http', function( $scope, $http )
 {
